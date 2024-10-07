cf24f4d0ec7dc37629778baaf7310586682de83c
LOKESHLOKI07 <103240327+LOKESHLOKI07@users.noreply.github.com>
2024-10-07 15:25:00 +0530

    Update urls.py

--- data_pusher/data_pusher/urls.py
+++ data_pusher/data_pusher/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for data_pusher project.
+URL configuration for data_pusher project
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
